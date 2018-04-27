2ce914e36395ffc675d370c0bcc132a54a349560
Ashok1233 <37237596+Ashok1233@users.noreply.github.com>
2018-04-27 09:40:32 +0530

    Update DemoA.java

--- src/Demos/DemoA.java
+++ src/Demos/DemoA.java
@@ -3,7 +3,7 @@ package Demos;
 public class DemoA {
 
 	public static void main(String[] args) {
-		System.out.println("hi ..Ashokkumar and bharat");
+		System.out.println("hi ..Ashokkumar and bharat ...");
 
 	}
 
